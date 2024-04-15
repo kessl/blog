32d1163d7fbd50bd40e5ca5072f2d4e3f98265b2
Daniel Kessl <dan@kessl.net>
2024-04-15 22:33:32 +0200

    Spawn on mouseover

--- assets/world.js
+++ assets/world.js
@@ -1,5 +1,5 @@
-const rows = 50
-const columns = 80
+const rows = 60
+const columns = 100
 
 // Even row
 //         (-1, 0)
@@ -17,13 +17,30 @@ const evenRowNeighborCoords = [[0, -1], [-1, 0], [0, 1], [1, -1], [1, 0], [1, 1]
 //          (1, 0)
 const oddRowNeighborCoords = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [1, 0], [0, 1]]
 
-let running = false
-let nextFrame
 let lastFrameTime = 0
-let generationCount = 0
+
+const hoverBuffer = []
+let hoverBufferTimeout = null
 
 const currentGen = []
 const nextGen = []
+
+function flushHoverBuffer() {
+  hoverBuffer.length = 0
+  console.log('flushed')
+}
+
+function handleMouseoverCell(event) {
+  const cell = event.target
+  const [x, y] = cell.id.split(',').map(Number)
+
+  hoverBuffer.push([x, y])
+  currentGen[y][x] = 1
+  cell.classList.add('alive')
+
+  clearTimeout(hoverBufferTimeout)
+  hoverBufferTimeout = setTimeout(flushHoverBuffer, 200)
+}
 
 function createGenArrays() {
   for (let y = 0; y < rows; y++) {
@@ -35,14 +52,6 @@ function createGenArrays() {
     for (let x = 0; x < columns; x++) {
       currentGen[y][x] = 0
       nextGen[y][x] = 0
-    }
-  }
-}
-
-function randomize() {
-  for (let y = 0; y < rows; y++) {
-    for (let x = 0; x < columns; x++) {
-      currentGen[y][x] = Math.random() > 0.8 ? 1 : 0
     }
   }
 }
@@ -59,7 +68,7 @@ function createWorld() {
       cell.classList.add('hex')
       cell.classList.toggle('even', x % 2 === 0)
       cell.id = `${x},${y}`
-      cell.addEventListener('click', handleClickCell)
+      cell.addEventListener('mouseover', handleMouseoverCell)
       row.appendChild(cell)
     }
     world.appendChild(row)
@@ -89,11 +98,9 @@ function getNeighborCount(row, column) {
   let y, x
   for (let i = 0; i < 6; i++) {
     if (column % 2) {
-      y = evenRowNeighborCoords[i][0]
-      x = evenRowNeighborCoords[i][1]
+      [y, x] = evenRowNeighborCoords[i]
     } else {
-      y = oddRowNeighborCoords[i][0]
-      x = oddRowNeighborCoords[i][1]
+      [y, x] = oddRowNeighborCoords[i]
     }
 
     if ((row + y >= 0) && (row + y < rows) && (column + x >= 0) && (column + x < columns)) {
@@ -106,6 +113,7 @@ function getNeighborCount(row, column) {
 function updateCurrentGen() {
   for (row in currentGen) {
     for (col in currentGen[row]) {
+      if (hoverBuffer.includes([row, col])) continue
       currentGen[row][col] = nextGen[row][col]
       nextGen[row][col] = 0
     }
@@ -123,58 +131,17 @@ function updateWorld() {
 
 function simulate(timestamp) {
   const deltaTime = timestamp - lastFrameTime
-
   if (deltaTime > 100) {
     createNextGen()
     updateCurrentGen()
     updateWorld()
-
-    generationCount++
-    document.querySelector('#gen').innerHTML = generationCount
-
     lastFrameTime = timestamp
   }
-
-  if (running) {
-    nextFrame = requestAnimationFrame(simulate)
-  }
-}
-
-function handleStartStop(){
-  const button = document.querySelector('#start-stop')
-
-  if (!running) {
-    running = true
-    button.innerText = 'Stop'
-    simulate()
-  } else {
-    running = false
-    button.innerText = 'Start'
-    cancelAnimationFrame(nextFrame)
-  }
-}
-
-function handleRandomize() {
-  randomize()
-  updateWorld()
-}
-
-function handleClickCell(event) {
-  const cell = event.target
-  const [x, y] = cell.id.split(',').map(Number)
-
-  if (cell.classList.contains('alive')) {
-    cell.classList.remove('alive')
-    currentGen[y][x] = 0
-  } else {
-    cell.classList.add('alive')
-    currentGen[y][x] = 1
-  }
+  requestAnimationFrame(simulate)
 }
 
 window.onload = function () {
   createWorld()
   createGenArrays()
-  document.querySelector('#start-stop').addEventListener('click', handleStartStop)
-  document.querySelector('#randomize').addEventListener('click', handleRandomize)
+  simulate()
 }
